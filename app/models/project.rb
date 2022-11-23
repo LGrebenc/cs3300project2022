5dbbf161b12ea7bc6054465a1f018a857cc95f27
JLG docker <ljgreb2001@gmail.com>
2022-11-23 21:36:18 +0000

    Success: test failed with line commented out; Uncommenting line now

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,5 +2,5 @@ class Project < ApplicationRecord
     #validates :title, presence: true
 
     # require title and description
-    #validates_presence_of :title, :description
+    validates_presence_of :title, :description
 end
